133aca7569d64726dda0736523b11e019608a07f
sagu2000 <68247677+sagu2000@users.noreply.github.com>
2025-08-17 14:56:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: "/portfolio/",   
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/portfolio/", 
 });
